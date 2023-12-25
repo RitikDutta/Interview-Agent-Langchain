bc43e22cab54d8d440185f4a86b0dcf53e5e10c8
ritik dutta <ritikduttagd@gmail.com>
2023-12-25 12:23:39 +0530

    added additional db functions

--- database/user_manager.py
+++ database/user_manager.py
@@ -20,3 +20,48 @@ class UserManager:
             performance = {}  # Initialize performance data, if any
             user_data = {'details': details, 'performance': performance}
             self.firestore_crud.create_document(user_id, user_data)
+
+
+    def add_thread_id(self, user_id, thread_id):
+        # Add thread_id to the user's performance data
+        user_data = self.firestore_crud.read_document(user_id)
+        if user_data:
+            user_data['performance']['thread_id'] = thread_id
+            self.firestore_crud.update_document(user_id, user_data)
+
+    def get_thread_id(self, user_id):
+        # Retrieve thread_id for the given user_id
+        user_data = self.firestore_crud.read_document(user_id)
+        if user_data and 'performance' in user_data:
+            return user_data['performance'].get('thread_id')
+        return None
+
+
+    def add_assistant_id(self, user_id, assistant_id):
+        # Add assistant_id to the user's performance data
+        user_data = self.firestore_crud.read_document(user_id)
+        if user_data:
+            user_data['performance']['assistant_id'] = assistant_id
+            self.firestore_crud.update_document(user_id, user_data)
+
+    def get_assistant_id(self, user_id):
+        # Retrieve assistant_id for the given user_id
+        user_data = self.firestore_crud.read_document(user_id)
+        if user_data and 'performance' in user_data:
+            return user_data['performance'].get('assistant_id')
+        return None
+
+
+    def add_or_update_score(self, user_id, score):
+        # Add or update the score in the user's performance data
+        user_data = self.firestore_crud.read_document(user_id)
+        if user_data:
+            user_data['performance']['score'] = score
+            self.firestore_crud.update_document(user_id, user_data)
+
+    def get_score(self, user_id):
+        # Retrieve the score for the given user_id
+        user_data = self.firestore_crud.read_document(user_id)
+        if user_data and 'performance' in user_data:
+            return user_data['performance'].get('score')
+        return None
